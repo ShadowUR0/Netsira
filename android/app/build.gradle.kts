7c2b483f38f48ac785c43a8ae7b7357a583cf4c9
Shadow <153516052+ShadowUR0@users.noreply.github.com>
2026-09-22 13:08:26 +0300

    [release] Signed Android release APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "io.github.shadowur0.netsira"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.0-preview.4"
+        versionCode = 5
+        versionName = "0.3.0-preview.5"
     }
     buildTypes {
         getByName("release") {
